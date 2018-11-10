d5719a88472fccc477e7e2908f349676e1c2b78d
mdshun <hoang_van_tuan@mediado.jp>
2018-11-10 20:05:05 +0900

    Fix log

--- log/log.go
+++ log/log.go
@@ -17,31 +17,27 @@ func init() {
 	}
 }
 
-// Logger is logger struct
-type Logger struct {
-}
-
 // Info is level info
-func (l *Logger) Info(msg string, fields ...zap.Field) {
+func Info(msg string, fields ...zap.Field) {
 	logger.Info(msg, fields...)
 }
 
 // Debug is level debug
-func (l *Logger) Debug(msg string, fields ...zap.Field) {
+func Debug(msg string, fields ...zap.Field) {
 	logger.Debug(msg, fields...)
 }
 
 // Warn is level warn
-func (l *Logger) Warn(msg string, fields ...zap.Field) {
+func Warn(msg string, fields ...zap.Field) {
 	logger.Warn(msg, fields...)
 }
 
 // Error is level error
-func (l *Logger) Error(msg string, fields ...zap.Field) {
+func Error(msg string, fields ...zap.Field) {
 	logger.Error(msg, fields...)
 }
 
 // Fatal is level fatal
-func (l *Logger) Fatal(msg string, fields ...zap.Field) {
+func Fatal(msg string, fields ...zap.Field) {
 	logger.Fatal(msg, fields...)
 }
